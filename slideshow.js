86f7f914b24582b10988a951414ac0cc02613daf
Angela Kochoska <a.kochoska@gmail.com>
2019-03-01 15:49:21 -0500

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -15,5 +15,5 @@ function showSlides() {
   // }
   slides[slideIndex-1].style.display = "block";  
   // dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 60 seconds
+  setTimeout(showSlides, 30000); // Change image every 60 seconds
 }
